d3076a55fc28f809fc75f0b05d8cbd6193f731f6
Rehatjot Kaur <rehatjot.k@gobindsarvar.ca>
2026-03-18 09:47:25 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "Rehatjot"
+age = 14
+school = "Gobind Sarvar School"
+city = "Surrey"
+print(name)
+print(age)
+print(school)
+print(city)
+
+name = "Rehatjot"
+age = 14
+booleans = True
+num = 3.14
+print(name)
+
